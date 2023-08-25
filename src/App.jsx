f0aa051f4a48e4e5f62abecf0e11dff408fd3188
HimanshusinghRaghav <hsinghraghav1@gmail.com>
2023-08-25 13:52:51 +0530

    add:-"product route"

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AdminLogin from './pages/AdminLogin'
 import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import AddProducts from './pages/AddProducts';
 function App() {
 
   localStorage.setItem("AUTH_TOKEN" , '')
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path='/adminlogin' element={<AdminLogin purpose="login"/>}/>
           <Route exact path='/adminsignup' element={<AdminLogin purpose="signup"/>}/>
+          <Route exact path='/' element={<AddProducts/>}/>          
         </Routes>
     </Router>
     <ToastContainer className="z-50" />
